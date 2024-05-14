a26f4526e33201b18bec44cc01416aa06855f9be
danehoov <DaneHoov22@gmail.com>
2024-05-14 15:53:06 -0600

    linked timing

--- aa05-linked-list-basics-temp/linked-list-timing.js
+++ aa05-linked-list-basics-temp/linked-list-timing.js
@@ -1,9 +1,59 @@
-const LinkedList = require('./linked-list.js');
-const DoublyLinkedList = require('./doubly-linked-list.js');
+const LinkedList = require("./linked-list.js");
+const DoublyLinkedList = require("./doubly-linked-list.js");
 
 /*
 Construct a timing test to verify the time complexities of `addToHead` and
 `addToTail` for both singly and doubly linked lists.
 */
 
-// Your code here 
+let heads = [];
+let tails = [];
+
+for (let i = 0; i < 100; i++) {
+  heads.push(i);
+  tails.push(i);
+}
+
+let list1 = new LinkedList();
+
+console.log("\n====== LinkedList: addToHead ======\n");
+let startTime;
+let endTime;
+heads.forEach((node) => {
+  startTime = Date.now();
+  list1.addToHead(node);
+  endTime = Date.now();
+
+  console.log(node, endTime - startTime);
+});
+
+console.log("\n====== LinkedList: addToTails ======\n");
+tails.forEach((node) => {
+  startTime = Date.now();
+  list1.addToTail(node);
+  endTime = Date.now();
+
+  console.log(node, endTime - startTime);
+});
+
+let list2 = new DoublyLinkedList();
+
+console.log("\n====== Dub-LinkedList: addToHead ======\n");
+heads.forEach((node) => {
+  startTime = Date.now();
+  list2.addToHead(node);
+  endTime = Date.now();
+
+  console.log(endTime - startTime);
+});
+
+console.log("\n====== Dub-LinkedList: addToTails ======\n");
+tails.forEach((node) => {
+  startTime = Date.now();
+  list2.addToTail(node);
+  endTime = Date.now();
+
+  console.log(endTime - startTime);
+});
+
+// both returned constant timing
